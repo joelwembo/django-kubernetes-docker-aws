4d0bee69b5b3eae793da6eebb7938260ff53e288
Joel Wembo <joelotepawembo@gmail.com>
2022-10-24 05:46:41 +0800

    updated settings

--- apps/api_app/serializers.py
+++ apps/api_app/serializers.py
@@ -24,9 +24,9 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'name']
         
 class PersonSerializer(serializers.ModelSerializer):
-       class Meta:
-            model = Person
-            fields = ('name', 'birth_year', 'eye_color', 'species')
+    class Meta:
+        model = Person
+        fields = ('name', 'birth_year', 'eye_color', 'species')
 
 
 class SpeciesSerializer(serializers.ModelSerializer):
