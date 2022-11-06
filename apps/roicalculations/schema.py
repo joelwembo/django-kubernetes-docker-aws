1f9ea578f00568cc79f80a83a87a29e8385d5954
Joel Wembo <joelotepawembo@gmail.com>
2022-11-06 15:50:57 +0800

    roi calculations update

---
+++ apps/roicalculations/schema.py
@@ -0,0 +1,18 @@
+from .models import Contact
+from ninja import ModelSchema
+
+
+class ContactBase(ModelSchema):
+    class Config:
+        model = Contact
+        model_fields = ['first_name', 'last_name', 'organisation', 'phone_number']
+
+# schema for contact creaton
+class ContactCreate(ContactBase):
+    pass
+
+
+class Contact(ModelSchema):
+    class Config:
+        model = Contact
+        model_fields = ['id','first_name', 'last_name','organisation', 'phone_number', 'created_on']
--- apps/roicalculations/views.py
+++ apps/roicalculations/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
 import sys
 import datetime as date
 from datetime import *
+from .models import Contact as contact_table
+
 
 
 # Create your views here.
@@ -172,3 +172,30 @@ def emf(request, amount, duration):
     context = {'amount': investamount, 'ir': ir, 'period': duration, 'totalinterest': totalinterest,
                'totalROIA': totalROIA, 'totalinvest': totalinvest, 'oneROIA': oneROIA, 'oneinterest': oneinterest}
     return render(request, 'renderresult.html', context)
+
+
+class Contact:
+    def __init__(self):
+        pass
+
+    def create_contact(request, contact):
+        try:
+            if type(contact) != "dict":
+                contact = contact.dict()
+            contact = contact_table.objects.create(**contact)
+            contact.save()
+            if contact:
+                return "contact saved"
+            return {"error ": "failed to save"}
+        except Exception as error:
+            return str(error)
+
+    def get_contacts(request):
+        try:
+            contacts = contact_table.objects.all()
+            return contacts
+            if contacts:
+                return contacts
+            return {status: 500, context: "failed to save"}
+        except Exception as error:
+            return str(error)
